84df0022267e078a4a662f94683b300e4043e922
sonyalapina <crolicaa@gmail.com>
2024-09-21 09:54:54 +0300

    Замена на разность

--- helloworld.py
+++ helloworld.py
@@ -3,6 +3,6 @@ print("Hello world!")
 a = int(input("Ведите целое число: "))
 b = int(input("Ведите ещё одно: "))
 
-summa = a + b
-print("Сумма:", summa)
+raznost = a - b
+print("Разность:", raznost)
 
